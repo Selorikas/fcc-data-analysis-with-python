4b25b949ce905128f07c3bf0d38689ce2a4bc932
Thomas <selorikas@gmail.com>
2022-07-29 19:27:59 +0200

    add complete mvsd calculator

---
+++ mean-variance-standard_deviation-calculator/mean_var_std.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def calculate(list):
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+
+    matrix = __create_matrix(list)
+    
+    calculations = dict()
+    calculations["mean"] = __calculate_measure(matrix, np.mean)
+    calculations["variance"] = __calculate_measure(matrix, np.var)
+    calculations["standard deviation"] = __calculate_measure(matrix, np.std)
+    calculations["max"] = __calculate_measure(matrix, np.amax)
+    calculations["min"] = __calculate_measure(matrix, np.amin)
+    calculations["sum"] = __calculate_measure(matrix, np.sum)
+    return calculations
+
+def __create_matrix(list):
+    return np.reshape(list,(3,3))
+
+def __calculate_measure(matrix, fn):
+    list = []
+    list.append(fn(matrix, axis=0).tolist()) # axis1 / columns
+    list.append(fn(matrix, axis=1).tolist()) # axis2 / rows
+    list.append(fn(matrix).tolist()) # flattened
+    return list
